174b5a6875a425e7976621d47dda45a6e10b6692
deoramadanu <deo121005@gmail.com>
2025-06-24 11:08:09 +0700

    step 3

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -39,6 +39,12 @@ void insert()
         // step 2: Assign value to the data field of new node
         newNode->info = x;
 
+        // step 3: Make the left and the right child of the new node point to null 
+        newNode->leftchild = nullptr;
+        newNode->rightchild = nullptr;
+
+        
+
         
 
         
