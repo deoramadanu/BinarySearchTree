4b21ec6ecce27b0bb6e86f3682184f32b8d1f3d9
deoramadanu <deo121005@gmail.com>
2025-06-24 11:10:42 +0700

    step 7, 7a, 7b

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -66,6 +66,19 @@ void insert()
             // 6b: exit
             return;
         }
+        // step 7: if the value in the data field of the new node is greater than that of the parent 
+        else if ( x = parent->info)
+        {
+            // 7a: make the right child of parent pointer to the new node 
+            parent->rightchild = newNode;
+
+            // 7b: exit 
+            return;
+        }
+    }
+
+    
+
 
         
         
